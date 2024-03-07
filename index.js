425e45c738023e6053ef464e99de916bce579a8e
Lucia Gaudioso <Lucia.G@iclgo.com>
2024-03-07 19:26:22 +0000

    Feature completed: posting and creating users and exercised

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var exerciseSchema = new mongoose.Schema({
   userId: {type: String, required: true},
   description: {type: String, required: true},
   duration: {type: Number, required: true},
-  date: {type: Date, default: Date.now}
+  date: {type: Date, default: () => new Date()}
 })
 var exercise = mongoose.model("exercise",exerciseSchema);
 
@@ -40,7 +40,32 @@ let data = await user.find({})
 res.json(data);
 })
 
+app.post("/api/users/:_id/exercises", async (req,res)=>{
+  let id = req.params._id;
+  let description = req.body.description;
+  let duration = req.body.duration;
+  let date = new Date(req.body.date);
+  console.log("req.body.date is",req.body.date);
 
+  console.log("the date is",date);
+  let savedExercise = await new exercise({description: description, duration: duration, userId: id}).save();
+  console.log("saved exercise is",savedExercise.toJSON());
+  if(!(date == "Invalid Date")){ 
+    savedExercise.date = new Date(date);  
+     await savedExercise.save();
+    }
+  console.log("savedExercise.date is",savedExercise.date);
+  let foundUser = await user.findOne({_id: id});
+  foundUser.log.push(savedExercise);
+  await foundUser.save();
+  console.log("savedExercise.date is",savedExercise.date);
+  let dateString = savedExercise.date.toDateString();
+  console.log("dateString is",dateString);
+  let count = foundUser.log.length;
+  //console.log( " Count is %d, date is %s",count,dateString);
+  res.json({_id: foundUser._id, username: foundUser.username, date:dateString, duration: savedExercise.duration, description: savedExercise.description, });
+ // res.json({username: foundUser.username, count: count, _id: foundUser._id, log:[{description: savedExercise.description, duration: savedExercise.duration, date: dateString,}],});
+  })
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
